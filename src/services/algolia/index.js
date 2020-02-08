09de69138f02abddfe3dae2b121cc5de2ee82dcd
sbarcenas <SBarcenas_@outlook.com>
2020-02-08 10:52:07 -0500

    Prev Algolia

--- src/services/algolia/index.js
+++ src/services/algolia/index.js
@@ -4,6 +4,8 @@ import {APPLICATION_ID, API_KEY} from '../../utils/constants';
 export const algoliaClient = algoliasearch(APPLICATION_ID, API_KEY);
 
 export const searchClient = algoliasearch(
-    '0X6V7G3F7X',
-    '0d50ed18bc93be8ac7d71c8ecc6fb9c2',
+  '0X6V7G3F7X',
+  '0d50ed18bc93be8ac7d71c8ecc6fb9c2',
 );
+
+const index = algoliaClient.initIndex('your_index_name');
